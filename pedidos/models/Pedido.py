9408cfb1e7ed34296cb0de9b6dbb2db6ddf8792c
LucasBergaminTurtle <developer.01@turtlebp.com>
2023-05-30 16:05:58 -0300

    Adicionando Sequencial em Pedido

--- pedidos/models/Pedido.py
+++ pedidos/models/Pedido.py
@@ -7,8 +7,14 @@ class Pedido(models.Model):
     _name = 'pedidos.pedido'
     _description = 'Pedido'
 
+    name = fields.Char(
+        string="Número do Pedido",
+        readonly=True
+    )
+
     client_name = fields.Char(
-        string="Nome do cliente"
+        string="Nome do cliente",
+        required=True
     )
 
     order_date = fields.Date(
@@ -31,3 +37,8 @@ class Pedido(models.Model):
         inverse_name="pedido_id",
         string="Produtos"
     )
+
+    @api.model
+    def create(self, values):
+        values['name'] = self.env['ir.sequence'].next_by_code('seq.pedido')
+        return super(Pedido, self).create(values)
